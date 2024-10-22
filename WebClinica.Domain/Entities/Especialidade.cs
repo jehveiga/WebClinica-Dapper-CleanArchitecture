96780e57a8eabb91a0fc1e4c0a0ebdef7ea43b7a
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-10-22 14:35:52 -0300

    Adicionando as entidade Especialidade e Medico

---
+++ WebClinica.Domain/Entities/Especialidade.cs
@@ -0,0 +1,19 @@
+﻿namespace WebClinica.Domain.Entities
+{
+    public class Especialidade
+    {
+        public int IdEsp { get; }
+        public string Nome { get; private set; }
+
+        public Especialidade(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Especialidade(int id_Esp, string nome)
+        {
+            IdEsp = id_Esp;
+            Nome = nome;
+        }
+    }
+}
---
+++ WebClinica.Domain/Entities/Medico.cs
@@ -0,0 +1,30 @@
+﻿namespace WebClinica.Domain.Entities
+{
+    public class Medico
+    {
+        public Medico(int crm, string nome, char sexo, DateTime dataNascimento)
+        {
+            Crm = crm;
+            Nome = nome;
+            Sexo = sexo;
+            DataNascimento = dataNascimento;
+        }
+
+        public Medico(int crm, string nome, char sexo, DateTime dataNascimento, int especialidadeId, Especialidade especialidade)
+        {
+            Crm = crm;
+            Nome = nome;
+            Sexo = sexo;
+            DataNascimento = dataNascimento;
+            EspecialidadeId = especialidadeId;
+            Especialidade = especialidade;
+        }
+
+        public int Crm { get; private set; }
+        public string Nome { get; set; }
+        public char Sexo { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public int EspecialidadeId { get; private set; }
+        public Especialidade? Especialidade { get; set; }
+    }
+}
